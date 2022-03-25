d2c004de92e0c4d56cfc7c7e77b49f7ac7ae0900
Yuni <syjeong215@gmail.com>
2022-03-25 02:23:39 -0400

    Initial commit

---
+++ app/src/main/java/com/example/instagram/App.kt
@@ -0,0 +1,27 @@
+package com.example.instagram
+import android.app.Application
+import android.util.Log
+import com.parse.Parse
+import com.parse.ParseObject
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Parse.initialize(
+            Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build());
+
+        val firstObject = ParseObject("FirstClass")
+        firstObject.put("message","Hey ! First message from android. Parse is now connected")
+        firstObject.saveInBackground {
+            if (it != null){
+                Log.e("App", "Error saving object")
+            }else{
+                Log.d("MainActivity","Object saved.")
+            }
+        }
+    }
+}
